0d74db21f3747e553c516a9af7432921f8a7ba1d
Damlrca <sir.damirca@yandex.ru>
2023-09-12 20:46:21 +0300

    QuickSort

--- QuickSort/QuickSort.h
+++ QuickSort/QuickSort.h
@@ -2,14 +2,28 @@
 #define AiAS_QuickSort
 #include "Utils.h"
 
-template<typename Iter, typename Comp = Less<>>
+template<class Iter, class Comp = Less<>>
 void QuickSort(Iter first, Iter last, Comp comp = Comp{}) {
-	// TODO
+	while (first != last) {
+		Iter q = Partition(first, last, comp);
+		QuickSort(first, q, comp);
+		first = ++q;
+	}
 }
 
-template<class RandomIt, class Compare>
-RandomIt Pratition(RandomIt first, RandomIt last, Compare comp) {
-	// TODO
+template<class Iter, class Comp>
+Iter Partition(Iter first, Iter last, Comp comp) {
+	--last;
+	auto x = *last;
+	Iter q = first;
+	for (Iter j = first; j != last; ++j) {
+		if (comp(*j, x)) {
+			Swap(*q, *j);
+			++q;
+		}
+	}
+	Swap(*q, *last);
+	return q;
 }
 
 #endif // !AiAS_QuickSort
