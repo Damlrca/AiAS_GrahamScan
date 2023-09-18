4bc306a1815b3a87972abc020129cd08fa3f257e
Damlrca <sir.damirca@yandex.ru>
2023-09-18 19:15:21 +0300

    test test test

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -12,22 +12,23 @@ using namespace std::chrono;
 
 int main() {
 	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dist(1, 1'000'000);
+	std::mt19937_64 gen(rd());
+	std::uniform_int_distribution<long long> dist(1, 1'000'000'000);
 	using clock = chrono::system_clock;
 
-	ofstream out("TestRandom.txt");
+	ofstream out("TestReversed64.txt");
 	for (int i = 1; i <= 1'000'001; i += 10'000) {
-		vector<int> v(i);
-		for (auto& j : v)
-			j = dist(gen);
+		vector<long long> v(i);
+		//for (auto& j : v)
+			//j = dist(gen);
+		iota(v.rbegin(), v.rend(), 1ll);
 		long long time = 0;
 		int k = 10;
 		while (k--) {
 			for (auto& j : v)
 				j = dist(gen);
 			auto start_time = clock::now();
-			AVLTree<int>(v.begin(), v.end()).data();
+			AVLTree<long long>(v.begin(), v.end()).data();
 			auto end_time = clock::now();
 			time += duration_cast<milliseconds>(end_time - start_time).count();
 		}
